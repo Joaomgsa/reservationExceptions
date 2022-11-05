370a16f6fbbd7963ddeacc7b2f2946445bdb7a75
Joaomgsa <joaomgsa@gmail.com>
2022-11-05 17:38:21 -0300

    ajustando arquivo readme

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,6 +1,46 @@
 package model.entities;
 
+import java.util.Date;
+
 public class Reservation {
-    private Integer roomNumbem;
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    public Reservation(){
+
+    }
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Date checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    // falta implementar a regra de negocio
 
 }
